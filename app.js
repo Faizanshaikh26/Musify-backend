ff8bbe845a2b7970d1bfe115100bb9c2ea11178f
Faizanshaikh26 <Faizansheikh8209@gmail.com>
2024-05-31 16:42:06 +0530

    cors

--- app.js
+++ app.js
@@ -2,9 +2,10 @@ const express = require('express');
 const app = express();
 require('./db/connection');
 const userControllers = require('./Controllers/User');
+const cors=require("cors")
 
 app.use(express.json());
-
+app.use(cors());
 app.get('/', (req, res) => {
   res.send("Hey there");
 });
